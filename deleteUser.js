7d48e70211a3fa5bb85bf1bc21e62ddd21f4395b
Abhinav Subramani <abhisubramani3@gmail.com>
2022-11-18 17:13:03 -0500

    delete users now working

---
+++ deleteUser.js
@@ -0,0 +1,37 @@
+const AWS = require('aws-sdk');
+const dynamoDocClient = new AWS.DynamoDB.DocumentClient();
+
+var response = false;
+
+const dynamoTable = process.env.TABLE_NAME;
+
+exports.handler = async (event) => {
+    console.log("Received event: ", JSON.stringify(event, 3));
+    try {
+        if (event.info.fieldName=="deleteUser"){
+            await deleteUser(event.arguments);
+            response = true;
+        }
+        
+    } catch (e) {
+        console.log(e)
+    }
+    console.log(response);
+    return response;
+};
+
+const deleteUser = (item) => {
+    return new Promise((resolve,reject) => {
+        let params = {
+            TableName: dynamoTable,
+            Key: {
+                id: item.id,
+            }
+        };
+    
+        dynamoDocClient.delete(params, (err,data) => {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
+};
